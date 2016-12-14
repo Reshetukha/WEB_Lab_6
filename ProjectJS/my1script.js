c70ead9dd7b8512c8e466264444255659cdba00d
AFRIKAKiev <sslavaa94@mail.ru>
2016-12-14 22:51:32 +0200

    1

---
+++ ProjectJS/my1script.js
@@ -0,0 +1,12 @@
+//window.jQuery = window.$ = jQuery;
+
+/*$( document ).ready(function() {
+  console.log( 'готов!' );
+  alert("ready!");
+});*/
+
+
+//window.jQuery = window.$ = jQuery;
+
+console.log("hello!");
+alert("hello!");
